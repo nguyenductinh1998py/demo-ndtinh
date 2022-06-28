8357bdb0d4f9551d092e99b7ef85ae0d2538674e
Nguyen Duc Tinh <ndtinh@tma.com.vn>
2022-06-28 17:54:19 +0700

    handing unit test

--- demo/src/test/java/com/ndtinh/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/ndtinh/demo/DemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.ndtinh.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ndtinh.demo.controller.BookController;
 
 @SpringBootTest
 class DemoApplicationTests {
+	
+	
+	@Autowired
+	private BookController bookController;
 
 	@Test
 	void contextLoads() {
+		assertThat(bookController).isNotNull();
+	}
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
 	}
 
 }
